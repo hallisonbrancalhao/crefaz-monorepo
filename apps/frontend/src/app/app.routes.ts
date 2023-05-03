baf72b921c1a9bdd492a5749488e955b2f90685a
Hallison <hallison.o.b@gmail.com>
2023-05-03 15:46:04 -0300

    fix: routes

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
-    path: 'shared-ui-layout',
+    path: '',
     loadChildren: () =>
       import('@crefaz-monorepo/shared/ui-layout').then(
         (m) => m.SharedUiLayoutModule
