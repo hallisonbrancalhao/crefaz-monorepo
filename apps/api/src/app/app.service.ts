46df2daf6c3e4f22ab547018988469534d261cf2
Hallison <hallison.o.b@gmail.com>
2023-05-12 23:32:31 -0300

    feat: insert api data

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { UserSession } from '@crefaz-monorepo/shared/data-access';
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return ({ message: 'Hello API' });
+    return { message: 'Hello API' };
+  }
+
+  getSession(): UserSession {
+    const data: UserSession = {
+      token: 'ASDFKLASDG1568DG69846846AE8V4WE849',
+      expires: Date.now(),
+      models: [
+        {
+          route: 'dashboard',
+          icon: 'fal fa-home',
+          label: 'Dashboard',
+          permission: 3,
+        },
+        {
+          route: 'statistics',
+          icon: 'fal fa-chart-bar',
+          label: 'Estatísticas',
+          permission: 3,
+        },
+        {
+          route: 'coupens',
+          icon: 'fal fa-tags',
+          label: 'Dados',
+          permission: 3,
+        },
+        {
+          route: 'pages',
+          icon: 'fal fa-file',
+          label: 'Páginas',
+          permission: 3,
+        },
+        {
+          route: 'settings',
+          icon: 'fal fa-cog',
+          label: 'Configurações',
+          permission: 3,
+        },
+      ],
+    };
+    return data;
   }
 }
--- libs/shared/data-access/src/lib/entities/user.ts
+++ libs/shared/data-access/src/lib/entities/user.ts
@@ -1,4 +1,17 @@
 export interface User {
-  id: number
-  name: string
+  id: number;
+  name: string;
+}
+
+export interface UserSession {
+  token: string;
+  expires: number;
+  models: Array<Models>;
+}
+
+export interface Models {
+  route: string;
+  icon: string;
+  label: string;
+  permission: number;
 }
